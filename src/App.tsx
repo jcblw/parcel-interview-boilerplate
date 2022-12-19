814002a2e30613c3c380b4822a64b51e69d8a195
Jacob Lowe <jacoblowe2.0@gmail.com>
2022-12-18 19:01:13 -0600

    add in some files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./app.css";
+
+export const App = () => {
+  return (
+    <div className="container">
+      <div className="home-hero--container">
+        <h1>Demo</h1>
+        <p>todo</p>
+      </div>
+    </div>
+  );
+};
